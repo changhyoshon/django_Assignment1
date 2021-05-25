4b7139dd4701ef8b5aae7654cd3d46b074f3438a
손창효 <ckdgy123@gmail.com>
2021-05-26 08:26:01 +0900

    first commit

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+SECRET_KEY = 'django-insecure-s)p=i_+z6xvobmllusyv!%0$wm98*uet6ua&fnrh*h^c8adxi-'
+
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crud_fst',
+        'USER': 'root',
+        'PASSWORD': 'Vs5453vs!',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
